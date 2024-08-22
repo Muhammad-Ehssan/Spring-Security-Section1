182dc51cb2fcf8cc36f1de78fa2c5783612e0c5a
mehssan <ehsanghazanfar2@gmail.com>
2024-08-22 23:40:51 +0500

    Added support for passwordEncoder and added multiple users authentication in-memory.

--- src/main/java/com/egr/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/egr/springsecurity/config/ProjectSecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -26,5 +32,16 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withUsername("user").password("{bcrypt}$2a$12$VXkHnP9e9m7zfCOZjGIkbum7gPTlW9eKAWNfT/XVDEIM.R3hg/MBC").authorities("read").build();
+        UserDetails userDetails1 = User.withUsername("admin").password("{noop}123").authorities("admin").build();
+        return new InMemoryUserDetailsManager(userDetails, userDetails1);
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
